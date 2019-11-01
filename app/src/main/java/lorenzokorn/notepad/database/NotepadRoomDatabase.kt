81f20c67d52c42dbdfec5a9550abb8634784f993
lorenzo korn <lkorn.9520@gmail.com>
2019-11-01 14:45:19 +0100

    added imports

--- app/src/main/java/lorenzokorn/notepad/database/NotepadRoomDatabase.kt
+++ app/src/main/java/lorenzokorn/notepad/database/NotepadRoomDatabase.kt
@@ -50,5 +50,4 @@ abstract class NotepadRoomDatabase : RoomDatabase() {
             return INSTANCE
         }
     }
-
 }
--- app/src/main/java/lorenzokorn/notepad/ui/MainActivity.kt
+++ app/src/main/java/lorenzokorn/notepad/ui/MainActivity.kt
@@ -1,16 +1,16 @@
 package lorenzokorn.notepad.ui
 
 import android.app.Activity
-import android.os.Bundle
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
-
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.content_main.*
 import lorenzokorn.notepad.R
 import lorenzokorn.notepad.database.MainActivityViewModel
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import com.androidcourse.notepadkotlin.R
+import com.androidcourse.notepadkotlin.ui.edit.EditActivity
 
 const val EDIT_REQUEST_CODE = 100
 
